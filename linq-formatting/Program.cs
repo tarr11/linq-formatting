38a745ce3972818323f7bb63117b8f8d09cc3e2d
Douglas Tarr <douglas.tarr@gmail.com>
2013-10-24 11:18:22 -0700

    Sample app

---
+++ linq-formatting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_formatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new Foo();
+            var result = foo.Bars
+        .OrderBy(s => s.Baz)
+            .Select(s => s.Baz);
+
+        }
+    }
+
+    public class Foo
+    {
+        public ICollection<Bar> Bars { get; set; }
+
+    }
+
+    public class Bar
+    {
+        public string Baz { get; set; }
+
+    }
+
+
+}
